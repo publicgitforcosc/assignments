d5782c4fe20116297f058a6a97ff0a3ff64fb337
cg <c@g.com>
2022-08-22 07:02:49 +1200

    get user details

--- StudyShare/HomeViewController.swift
+++ StudyShare/HomeViewController.swift
@@ -2,31 +2,38 @@
 //  HomeViewController.swift
 //  StudyShare
 //
-//  Created by Matthew Jennings on 31/07/22.
+//  Created by CGi on 21/08/22.
 //
-
 import UIKit
+import FirebaseAuth
+import FirebaseFirestore
 
 class HomeViewController: UIViewController {
-
     
     @IBOutlet weak var classTable: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        getUserDetails()
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
+    func getUserDetails(){
+        // https://www.anycodings.com/1questions/4119941/optimizing-get-from-firestore-in-swift
+        let db = Firestore.firestore()
+        if Auth.auth().currentUser != nil{
+            let user = Auth.auth().currentUser
+            User.UID = user!.uid
+           
+            let userDetails = db.collection("users").whereField("uid", isEqualTo: User.UID)
+            userDetails.getDocuments(){ (querySnaphot, err) in
+                if let err = err {
+                    print("error: \(err)")
+                } else{
+                    User.username = (userDataDict["username"] as! String)
+                }
+            }
+        } else {
+            print("error: authentification error") // debug!
+        }
     }
-    */
-
 }
